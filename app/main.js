ecfc56e47233b19c2b21234b35c1b12e2b6cdde2
Curt Newcomer <newcomercurt@gmail.com>
2015-09-20 19:00:02 -0700

    this is the setup for final lab

---
+++ app/main.js
@@ -0,0 +1,67 @@
+var course = {
+  "level": 201,
+  "name": "Foundations 2: JavaScript",
+  "lectures": [{
+    "topic": "Intro, JavaScript Basics 1",
+    "notes": ["The primitive data types are Boolean, Null, Undefined, Number, and String.",
+      "Parameters become variables inside their function.",
+      "A method is a function that is also the value of an object property."
+    ]
+  }, {
+    "topic": "JavaScript Basics 2 & Intro to Node",
+    "notes": ["Logical operators return the result of the last expression evaluated.",
+      "Loops execute the same instructions multiple times.",
+      "An infinite loop is a loop that never satisfies its exit condition."
+    ]
+  }, {
+    "topic": "Array Methods, Functions & Scope/Hoisting",
+    "notes": ["join() takes a delimiter, returns a string.",
+      "Generally useful code is often packaged up as a collection of functions and data in libraries.",
+      "JavaScript is function-scoped."
+    ]
+  }, {
+    "topic": "OOP in JavaScript and Classes/Inheritance",
+    "notes": ["object oriented programming allows programmers to organize data in a way that makes sense to them.",
+      "this is a reserved word in JavaScript that is used to refer to a specifc property of an object at the time of a function call.",
+      "Constructor functions are used to create object prototypes that can be used to create other objects that will inherit the properties of the prototype."
+    ]
+  }, {
+    "topic": "JavaScript in the Browser & Intro to jQuery",
+    "notes": ["jquery is a JavaScript library that makes performing interactive tasks on a webpage much easier",
+      "listeners can be used to respond to an action the user takes, there are many types of listeners",
+      "jquery can use different selectors to access parts of the dom"
+    ]
+  }, {
+    "topic": "Building a Server & Using Ajax",
+    "notes": ["Ajax is an asynchronous JavaScript that can be used to request and retrieve data without havint to reload a web page",
+      "npm stands for node package manager which has many different packages that are useful, examples are jshint, grunt, and expressjs",
+      " we can use express to build a server that can use request and respond with query selectors and Ajax udpate our page"
+    ]
+  }, {
+    "topic": "Build & Deploy an App",
+    "notes": ["During this lecture we made code used to search through these notes",
+      "We worked with Ajax more and had our page respond to our server"
+    ]
+  }, {
+    "topic": "Functional Programming with lodash",
+    "notes": ["lodash is another library for JavaScript that streamlines writing code",
+      "we only scratched the surface of lodash, but it is quite intriguing."
+    ]
+  }, ],
+  "labs": [{
+    "topic": "Beginning Loops, Arrays, and Functions",
+    "notes": ["in this lab we looped through an array to decide which animal to see at the zoo next",
+      "we also translated some meerkats"
+    ]
+  }, {
+    "topic": "Prototypes, more loops, functions, and Arrays",
+    "notes": ["in this lab we used a loop to calculate how long a town would be eaten by the blob",
+      "we also made a language translator for Romlulans, Klingons, and Humans"
+    ]
+  }, {
+    "topic": "Final Project",
+    "notes": ["figuring this one out",
+      "gonna do some searching"
+    ]
+  }]
+};
