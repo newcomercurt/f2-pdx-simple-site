08712133845867ccead47c4a9425a4face121e9f
Curt Newcomer <newcomercurt@gmail.com>
2015-09-26 12:19:29 -0700

    render search results

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -13,9 +13,40 @@ $("button").click(function() {
 
 $("form").on("submit", function(event) {
   event.preventDefault();
+  var form = this;
   $.get("search", {
-    search: this.searchText.value
+    search: form.searchText.value
   }).success(function(results) {
+    $("#search-results").empty();
+    if (results.phraseFound == false) {
+      alert("no results found for phrase: " + form.searchText.value)
+    } else {
+      displaySearchNotes(results)
+    }
     console.log(results)
   })
 });
+
+function displaySearchNotes(searchResults) {
+  var labs = searchResults.labs;
+  var lectures = searchResults.lectures;
+  if (labs.length) {
+    renderMarkupForSections(labs, "Labs");
+  }
+  if (lectures.length) {
+    renderMarkupForSections(lectures, "Lectures");
+  }
+};
+
+function renderMarkupForSections(sections, sectionTitle) {
+  var html = "<h1>" + sectionTitle + "</h1>"
+  sections.forEach(function(section) {
+    html += "<h3>" + section.topic + "</h3>";
+    html += "<ul>"
+    section.notes.forEach(function(note) {
+      html += "<li>" + note + "</li>";
+    })
+    html += "</ul>";
+  });
+  $("#search-results").append(html);
+}
